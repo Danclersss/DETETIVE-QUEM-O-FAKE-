bdab011c647365f66982e0f6c4ad8ad972c20fb9
Danclersss <128094454+Danclersss@users.noreply.github.com>
2026-03-22 18:44:06 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,14 @@
-const CACHE_NAME = 'v1';
+// Mudei o nome para v2 para forçar o celular a atualizar o cache!
+const CACHE_NAME = 'v2'; 
+
 const assets = [
   './',
   './index.html',
   './manifest.json',
-  './sw.js'
+  './sw.js',
+  // Adicionando os scripts do Firebase e as fontes para salvar offline!
+  'https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/9.22.1/firebase-database-compat.js'
 ];
 
 self.addEventListener('install', e => {
